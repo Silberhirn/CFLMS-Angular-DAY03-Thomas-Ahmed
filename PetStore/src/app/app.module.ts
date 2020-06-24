f2f37fdd48f07b0d55acf5c15158dbc91a10fa58
THomas Blieberger <thomas.blieberger@hotmail.com>
2020-06-24 13:13:22 +0200

    adds first steps

---
+++ PetStore/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-list/product-details/product-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    ListComponent,
+    ProductListComponent,
+    ProductDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ PetStore/src/app/products.ts
@@ -0,0 +1,34 @@
+
+export const products = [
+ {
+   name: 'Labormaus',
+   price: 1,
+   description: 'For testing purposes'
+ },
+ {
+   name: 'rare Snake',
+   price: 2000,
+   description: 'illegal to have'
+ },
+ {
+   name: 'Alligator',
+   price: 3000,
+   description: 'makes great purses/shoes/wallets'
+ },
+ {
+   name: 'Gorilla',
+   price: 0,
+   description: 'you pay for delivery'
+ },
+ {
+   name: 'Hamster',
+   price: 10,
+   description: 'may die on its first day'
+ },
+  {
+   name: 'Komodowaran',
+   price: 300,
+   description: 'Drache'
+ }
+
+];
